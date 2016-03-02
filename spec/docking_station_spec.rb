12cc4809d49e219cc3500eac2b055b4b74f604fe
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-02 15:54:33 +0000

    halfway through 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,18 +5,26 @@ describe DockingStation do
 
   describe '#release_bike' do
     subject {DockingStation.new.release_bike}
-    it  {is_expected.to be_a Bike}
+  
+    it "raises error when there are no bikes at the station" do
+      is_empty? = true
+      expect(subject).to raise_error("There are no bikes available")
+  end
+
+      it  {is_expected.to be_a Bike}
     it {is_expected.to be_working}
+    
   end
   describe '#dock_bike' do
     it "responds to method call of dock_bike" do
       expect(subject).to respond_to(:dock_bike).with(1).argument
     end
     it "when we dock a bike it returns that bike" do
-      bike = Bike.new
-      docking_a_bike  = subject.dock_bike(bike)
-      expect(docking_a_bike).to eq bike
+      docking_a_bike  = subject.dock_bike(subject.bike) 
+      expect(docking_a_bike).to eq subject.bike
     end
   end
+
+ 
 
 end
