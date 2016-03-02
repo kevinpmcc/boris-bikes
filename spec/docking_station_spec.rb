a22bc19bb5ec2bfead7f00b78db07b05fb101865
Hanif Fakira <fakira.work@gmail.com>
2016-03-02 12:50:00 +0000

    Q10 finished

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)}
+
+  describe '#release_bike' do
+    subject {DockingStation.new.release_bike}
+    it  {is_expected.to be_a Bike}
+    it {is_expected.to be_working}
+  end
 end
