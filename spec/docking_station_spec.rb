2aaf11711590c51c21457d2a5edfd84678f28711
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-03 12:01:37 +0000

    Challenge 8: refactord tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe '#release_bike'do
-    it "will respond to release_bike" do 
-       expect(subject).to respond_to(:release_bike) 
-    end
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike)  }
   end
 end
