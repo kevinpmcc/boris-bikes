1296fce220fee4fbb693157aa9923331838815a6
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-03 13:34:28 +0000

    Challenge 10: failing working? test.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,16 @@ require 'docking_station'
 
 describe DockingStation do
   describe '#release_bike' do
-    it { is_expected.to respond_to(:release_bike)  }
-  
-  #  it "releases a bike that is an instance of bike class"do
-    #  expect(subject.release_bike.class).to eq Bike
-    it { (is_expected.release_bike.class).to eq Bike }
-  end
-  
 
+    it { is_expected.to respond_to(:release_bike)  }
+    it "releases a bike that is an instance of bike class" do
+      expect(subject.release_bike.class).to eq Bike
+    end
+
+    it 'expects released bike to be working' do
+      expect(subject.release_bike.working?).to eq true
+    end
+
+  end
 
 end
---
+++ spec/feature_test.rb
@@ -0,0 +1,7 @@
+require './lib/docking_station'
+require './lib/bike'
+
+p station =  DockingStation.new
+
+p bike = station.release_bike
+p bike.working?
