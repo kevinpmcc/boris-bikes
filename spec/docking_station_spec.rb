2a7338bd07184b6a8537561ba104f5fe26e4e815
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-03 11:52:22 +0000

    challenge 8 failed unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  describe '#release_bike'do
+    it "will respond to release_bike" do 
+       expect(subject).to respond_to(:release_bike) 
+    end
+  end
 end
